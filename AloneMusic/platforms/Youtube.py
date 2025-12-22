fef8e228fb0c5615238f7c8ec4c47ed8524726ed
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-22 22:49:18 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -24,7 +24,7 @@ import random
 import logging
 from urllib.parse import quote_plus
 
-API_URL = "https://kartik.opusx.workers.dev/yt"
+API_URL = "https://shrutibots.site"
 DOWNLOAD_FOLDER = "downloads"
 CHUNK_SIZE = 8192  # don't chnge this value
 RETRIES = 6  # you may increase if error arises for some yt videos
